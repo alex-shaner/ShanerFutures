b910b5a2d419c8ea4365330d5e3203b28f734dbe
Alexander Shaner <amshaner07@gmail.com>
2019-07-25 23:10:40 -0400

    Add files via upload

--- src/Bar.java
+++ src/Bar.java
@@ -1,51 +1,50 @@
 public class Bar{
 	//I actually saw that there was a java "Date" class and was going to use it, but it's deprecated
-	public Object Date;
-	public Object Time;
-	public Object Open;
-	public Object High;
-	public Object Low;
-	public Object Close;
-	public Object Up;
-	public Object Down;
+	public String Date;
+	public String Time;
+	public Float Open;
+	public Float High;
+	public Float Low;
+	public Float Close;
+	public Integer Up;
+	public Integer Down;
 	
 	//constructor
 	public Bar(Object Date, Object Time, Object Open, Object High, Object Low, Object Close, Object Up, Object Down) {
-		this.Date = Date;
-		this.Time = Time;
-		this.Open = Open;
-		this.High = High;
-		this.Low = Low;
-		this.Close = Close;
-		this.Up = Up;
-		this.Down = Down;
+		this.Date = Date.toString();
+		this.Time = Time.toString();
+		this.Open = Float.parseFloat(Open.toString());
+		this.High = Float.parseFloat(High.toString());
+		this.Low = Float.parseFloat(Low.toString());
+		this.Close = Float.parseFloat(Close.toString());
+		this.Up = Integer.parseInt(Up.toString());
+		this.Down = Integer.parseInt(Down.toString());
 	}
 	
 	//get methods
 	public String getDate() {
-		return Date.toString();
+		return Date;
 	}
 	public String getTime() {
-		return Time.toString();
+		return Time;
 	}
 	public float getOpen(){
-		//this feels dirty
-		return Float.parseFloat(Open.toString());
+		return Open;
 	}
 	public float getHigh() {
-		return Float.parseFloat(High.toString());
+		return High;
 	}
 	public float getLow(){
-		return Float.parseFloat(Low.toString());
+		return Low;
 	}
 	public float getClose() {
-		return Float.parseFloat(Close.toString());
+		return Close;
 	}
 	public int getUp() {
-		return Integer.parseInt(Up.toString());
+		return Up;
 	}
 	public int getDown() {
-		return Integer.parseInt(Down.toString());
+		return Down;
 	}
 }
 
